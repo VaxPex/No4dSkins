f16438a66c4b94d1b0cf0e7ebb6ebff23a39fed8
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-05-22 17:53:17 +0300

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -10,50 +10,47 @@ use pocketmine\event\player\PlayerChangeSkinEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
 
-	protected function onEnable(): void{
+	protected function onEnable(): void
+	{
 		$this->saveResource("steve.json");
 		$this->saveResource("alex.json");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
-	public function onJoin(PlayerJoinEvent $event){
+	public function onJoin(PlayerJoinEvent $event)
+	{
 		$player = $event->getPlayer();
 		$skin = $player->getSkin();
-		if($skin->getGeometryName() === "geometry.humanoid.custom"){
-			$player->setSkin(new Skin($player->getSkin()->getSkinId(),
-				$player->getSkin()->getSkinData(),
-				$player->getSkin()->getCapeData(),
-				$player->getSkin()->getGeometryName(),
-				file_get_contents($this->getDataFolder() . "steve.json")));
-		}
-		if($skin->getGeometryName() === "geometry.humanoid.customSlim"){
-			$player->setSkin(new Skin($player->getSkin()->getSkinId(),
-				$player->getSkin()->getSkinData(),
-				$player->getSkin()->getCapeData(),
-				$player->getSkin()->getGeometryName(),
-				file_get_contents($this->getDataFolder() . "alex.json")));
-		}
+		$this->checkAndSetSkin($player, $skin);
 	}
 
-	public function onSkinChange(PlayerChangeSkinEvent $event){
+	public function onSkinChange(PlayerChangeSkinEvent $event)
+	{
 		$player = $event->getPlayer();
 		$newSkin = $event->getNewSkin();
-		if($newSkin->getGeometryName() === "geometry.humanoid.custom"){
-			$player->setSkin(new Skin($newSkin->getSkinId(),
-				$newSkin->getSkinData(),
-				$newSkin->getCapeData(),
-				$newSkin->getGeometryName(),
-				file_get_contents($this->getDataFolder() . "steve.json")));
-		}
-		if($newSkin->getGeometryName() === "geometry.humanoid.customSlim"){
-			$player->setSkin(new Skin($newSkin->getSkinId(),
-				$newSkin->getSkinData(),
-				$newSkin->getCapeData(),
-				$newSkin->getGeometryName(),
-				file_get_contents($this->getDataFolder() . "alex.json")));
-		}
+		$this->checkAndSetSkin($player, $newSkin);
 	}
 
+	private function checkAndSetSkin(Player $player, Skin $skin) {
+		if ($skin->getGeometryName() === "geometry.humanoid.custom") {
+			$player->setSkin(new Skin($player->getSkin()->getSkinId(),
+				$player->getSkin()->getSkinData(),
+				$player->getSkin()->getCapeData(),
+				$player->getSkin()->getGeometryName(),
+				file_get_contents($this->getDataFolder() . "steve.json"))
+			);
+			$player->sendSkin();
+		} else if ($skin->getGeometryName() === "geometry.humanoid.customSlim") {
+			$player->setSkin(new Skin($player->getSkin()->getSkinId(),
+				$player->getSkin()->getSkinData(),
+				$player->getSkin()->getCapeData(),
+				$player->getSkin()->getGeometryName(),
+				file_get_contents($this->getDataFolder() . "alex.json"))
+			);
+			$player->sendSkin();
+		}
+	}
 }
